ce564463226085defa9374965547dbd79f7b1933
Orta Therox <orta.therox@gmail.com>
2016-01-21 14:11:36 +0000

    Use the right keys for routes

--- Pod/Classes/Aerodramus.m
+++ Pod/Classes/Aerodramus.m
@@ -75,7 +75,7 @@
     }];
 
     _routes = [self mapDict:JSON[@"routes"] map:^id(NSDictionary *routeDict) {
-        return [[Route alloc] initWithName:routeDict[@"name"] route:routeDict[@"route"]];
+        return [[Route alloc] initWithName:routeDict[@"name"] route:routeDict[@"path"]];
     }];
 }
 
